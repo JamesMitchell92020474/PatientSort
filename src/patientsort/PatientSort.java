7bceab6191bc248381c40cb394abd95c148d097f
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-12 14:31:46 +1200

    PatientSort Initial

---
+++ src/patientsort/PatientSort.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patientsort;
+
+/**
+ *
+ * @title OptimisedBubbleSort.java
+ * @author James Mitchell 92020474
+ * @date 12/09/2019
+ * 
+ */
+
+public class PatientSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
